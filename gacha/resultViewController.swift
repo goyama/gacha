6f64b0e8a5cc50d32aa202ac5c5e3123e4617d3e
豪山本 <goyamamoto1998@gmail.com>
2016-06-05 23:36:32 +0900

    initial commit

---
+++ gacha/resultViewController.swift
@@ -0,0 +1,13 @@
+//
+//  resultViewController.swift
+//  gacha
+//
+//  Created by 豪山本 on 2016/06/05.
+//  Copyright © 2016年 山本豪. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: NSObject {
+
+}
